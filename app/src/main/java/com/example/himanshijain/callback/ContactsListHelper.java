f73c0ee822e80a3f1760b35a1fade4899116e650
HimanshiJain <himshijain@gmail.com>
2016-04-30 11:22:21 +0530

    update contacts on load application and buttn selectors

--- app/src/main/java/com/example/himanshijain/callback/ContactsListHelper.java
+++ app/src/main/java/com/example/himanshijain/callback/ContactsListHelper.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -94,19 +95,26 @@ public class ContactsListHelper extends SQLiteOpenHelper {
                 null, null, null, null, null);
         if (cursor != null)
             cursor.moveToFirst();
+
         if(cursor.moveToFirst()) {
-            Contact contact = new Contact(cursor.getString(cursor.getColumnIndex(CONTACT_ENTRY_ID)), cursor.getString(cursor.getColumnIndex(CONTACT_NAME_TITLE)),
-                    cursor.getString(cursor.getColumnIndex(CONTACT_NAME_PHONE_NUMBER)));
-            contacts.add(contact);
+
+            Contact updatedC=getUpdatedDetails(cursor.getString(cursor.getColumnIndex(CONTACT_ENTRY_ID)));
+            if(updatedC!=null){
+                updateDetails(updatedC);
+                Contact contact = new Contact(updatedC.id, updatedC.name,
+                    updatedC.phone_no);
+                contacts.add(contact);}
         }
 //        Log.i("number", cursor.getString(cursor.getColumnIndex(CONTACT_NAME_PHONE_NUMBER)));
 
 
         while(cursor.moveToNext()){
-            Contact contacti=new Contact(cursor.getString(cursor.getColumnIndex(CONTACT_ENTRY_ID)),cursor.getString(cursor.getColumnIndex(CONTACT_NAME_TITLE)),
-                    cursor.getString(cursor.getColumnIndex(CONTACT_NAME_PHONE_NUMBER)));
-            Log.i("number",cursor.getString(cursor.getColumnIndex(CONTACT_NAME_PHONE_NUMBER)));
-            contacts.add(contacti);
+            Contact updatedC=getUpdatedDetails(cursor.getString(cursor.getColumnIndex(CONTACT_ENTRY_ID)));
+            if(updatedC!=null){
+                updateDetails(updatedC);
+                Contact contact = new Contact(updatedC.id, updatedC.name,
+                        updatedC.phone_no);
+                contacts.add(contact);}
         };
         return contacts;
     }
@@ -139,5 +147,51 @@ public class ContactsListHelper extends SQLiteOpenHelper {
         }
         else
             return false;
+    }
+
+    public Contact getUpdatedDetails(String id){
+        Cursor cursor = context.getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                new String[]{id}, null);
+
+        if(cursor.moveToFirst())
+        {
+            String number=formatNumber(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+
+            Contact contact=new Contact(id,cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    number);
+            Log.i("details","id "+id+" name "+cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))+
+            " number "+number);
+            return contact;
+        }else return  null;
+    }
+
+    public String formatNumber(String number){
+        String num=number.replaceAll("\\s", "");
+        num=num.trim();
+        if(num.charAt(0)=='0')
+            num="+91"+num.substring(1);
+        else if(num.charAt(0)!='+')
+            num= "+91" + num;
+        return num;
+    }
+
+    public void updateDetails(Contact contact){
+
+            SQLiteDatabase db = this.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put(CONTACT_ENTRY_ID, contact.id);
+            values.put(CONTACT_NAME_TITLE, contact.name);
+            values.put(CONTACT_NAME_PHONE_NUMBER, contact.phone_no);
+            // Inserting Row
+            db.update(TABLE_CONTACTS,values,CONTACT_ENTRY_ID+"="+contact.id,null);
+
+            db.close(); // Closing database connection
+            Contact updated=getContact(contact.phone_no);
+            Log.i("details-updated","id "+updated.id+" number "+updated.phone_no+" name "+updated.name);
+
     }
 }
